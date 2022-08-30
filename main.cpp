54201db80d8b18c48d2aa1d3110d2e646f22e8d1
Nathanmsaxe <59581764+Nathanmsaxe@users.noreply.github.com>
2022-08-29 21:02:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 /*
  * main.cpp
  *
+ *  Created on: Aug 29, 2022
+ *      Author: student
+ */
+/*
+ * main.cpp
+ *
  *  Created on: Aug 23, 2022
  *      Author: Nathan Saxe
  */
@@ -22,8 +28,8 @@ struct LLnode{
 	Student theData;
 };
 
-void push_front(LLnode*, Student);
-void push_back(LLnode*, Student);
+void push_front(LLnode*&, Student);
+void push_back(LLnode*&, Student);
 int list_length(LLnode*);
 Student retrieve_front(LLnode*);
 Student retrieve_back(LLnode*);
@@ -38,10 +44,11 @@ int main() {
 	temp.studentName = "student1";
 	temp.studentID = 11111;
 	cout << "Main:  number of nodes in empty list " << list_length(theLLHeader1) << endl;
-	cout << "TEST" << endl;
 	cout << "Main:  contents of LL1 nodes" << endl;
 	display_nodes(theLLHeader1);
+
 	push_front (theLLHeader1, temp);
+
 //	push_front (theLLHeader2, {"Charlie",78901});	// not all compilers support this
 	cout << "Main:  number of nodes in list after 1 push - " << list_length(theLLHeader1) << endl;
 	cout << "Main:  contents of list after 1 push " << endl;
@@ -89,54 +96,83 @@ int main() {
 	return 0;
 }
 
-int list_length(LLnode * linkedListNode){
+int list_length(LLnode * p){
 	int counter = 0;
-	if(linkedListNode->fwdPtr==nullptr){
-		throw "list empty";
-
+	if(p!=nullptr){
+		while(p != nullptr){
+			counter++;
+			cout << p->theData.studentName << endl;
+			p = p->fwdPtr;
+		}
 	}
-
-	while(linkedListNode->fwdPtr!=nullptr){
-		linkedListNode->fwdPtr = linkedListNode->fwdPtr->fwdPtr;
-		counter++;
+	else{
+		return counter;
 	}
 
 	return counter;
+
 }
 
-void push_front(LLnode * linkedListNode, Student data){
-	int counter = 0;
-	while(linkedListNode[counter].fwdPtr!=nullptr){
-		counter++;
-	}
-	linkedListNode[counter].fwdPtr = new LLnode;
-	linkedListNode[counter+1].theData = data;
+void push_front(LLnode * &llh, Student data){
+	LLnode * newNode = new LLnode;
+	newNode->theData = data;
+	newNode->fwdPtr = llh;
+	llh = newNode;
+
 }
-void push_back(LLnode * linkedListNode, Student data){
-	if(linkedListNode->fwdPtr == nullptr){
-		linkedListNode->fwdPtr->theData = data;
+void push_back(LLnode * &llh, Student data){
+	LLnode * newNode = new LLnode;
+	LLnode * temp = new LLnode;
+	temp = llh;
+	newNode->theData = data;
+
+
+	newNode->fwdPtr = llh->fwdPtr;
+
+	//newNode->fwdllhtr = llh->fwdllhtr;
+	if(llh == nullptr){
+		llh = newNode;
+	}
+
+	while(temp!=nullptr){
+		temp = temp->fwdPtr;
+	}
+
+	temp = newNode;
+
+	newNode->fwdPtr = nullptr;
+
+
+}
+
+Student retrieve_front(LLnode * p){
+	if(p == nullptr){
+		throw 'A';
 	}
 	else{
-		while(linkedListNode->fwdPtr != nullptr){
-			linkedListNode->fwdPtr->fwdPtr = new LLnode;
-		}
-		linkedListNode->fwdPtr->fwdPtr->theData = data;
+		return p->theData;
 	}
-}
 
-Student retrieve_front(LLnode * linkedListNode){
-	return linkedListNode->theData;
 }
-Student retrieve_back(LLnode * linkedListNode){
-	while(linkedListNode->fwdPtr!=nullptr){
-		linkedListNode->fwdPtr = linkedListNode->fwdPtr->fwdPtr;
+Student retrieve_back(LLnode * p){
+	while(p!=nullptr){
+		p = p->fwdPtr;
 	}
-	return linkedListNode->theData;
+	return p->theData;
 }
-void display_nodes(LLnode * linkedListNode){
+void display_nodes(LLnode * p){
 	int counter = 0;
-	while(linkedListNode->fwdPtr!=nullptr){
-		cout << "node " << counter << "address -> " << linkedListNode->fwdPtr << "Student Name:" << linkedListNode->fwdPtr->theData.studentName << "Student ID: " << linkedListNode->fwdPtr->theData.studentName << endl;
-		counter++;
-	}
+		if(p!=nullptr){
+			while(p != nullptr){
+				counter++;
+				cout << "node " << counter << " address -> " << p << " Student Name:" << p->theData.studentName << " Student ID: " << p->theData.studentName << endl;
+				p = p->fwdPtr;
+			}
+		}
+		else{
+			cout << "Empty List" << endl;
+		}
 }
+//cout << "node " << counter << "address -> " << p->fwdPtr << "Student Name:" << p->fwdPtr->theData.studentName << "Student ID: " << p->fwdPtr->theData.studentName << endl;
+
+
